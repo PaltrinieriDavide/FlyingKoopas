13a633cacaff1b5f5c5bae9019bb8b9d1cff7f0f
DavidePaltrinieri <296723@studenti.unimore.it>
2023-06-21 14:30:19 +0200

    QuadLine inserita

--- src/main/java/com/example/dancingline/DancingLineController.java
+++ src/main/java/com/example/dancingline/DancingLineController.java
@@ -1,13 +1,29 @@
 package com.example.dancingline;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.shape.CubicCurve;
 import javafx.stage.Stage;
+import javafx.scene.shape.QuadCurve;
 
 public class DancingLineController {
 
     @FXML
-    private CubicCurve cubicCurve;
+    private QuadCurve quadCurve;
+
+    @FXML
+    private Button prova;
+
+    protected void updateCurve(double x1, double y1){
+        quadCurve.setControlX(x1);
+        quadCurve.setControlY(y1);
+    }
+
+    @FXML
+    void onButtunClick(ActionEvent event) {
+        updateCurve(-100,-100);
+    }
 
 }
---
+++ src/main/java/com/example/dancingline/SpriteBouncing.java
@@ -0,0 +1,66 @@
+package com.example.dancingline;
+
+import com.example.dancingline.motionelements.PVector;
+import com.example.dancingline.motionelements.Sprite;
+import javafx.scene.Node;
+
+public class SpriteBouncing extends Sprite {
+    public SpriteBouncing(Node view) {
+        super(view);
+    }
+
+    public SpriteBouncing(Node view, PVector location) {
+        super(view, location);
+    }
+
+    public SpriteBouncing(Node view, PVector location, PVector velocity) {
+        super(view, location, velocity);
+    }
+
+    public SpriteBouncing(Node view, PVector location, PVector velocity, PVector acceleration) {
+        super(view, location, velocity, acceleration);
+    }
+
+    public SpriteBouncing(Node view, PVector location, PVector velocity, PVector acceleration, double mass) {
+        super(view, location, velocity, acceleration, mass);
+    }
+
+    public void update() {
+        super.update();
+        reboundWalls();
+    }
+
+    public void reboundWalls() {
+        double offset;
+
+        // right wall
+        offset = getBoundsInParent().getMaxX() - getParent().getLayoutBounds().getMaxX();
+        if (offset > 0) {
+            getLocation().x -= offset;
+            getVelocity().x *= -1;
+        }
+
+        // left wall
+        offset = getBoundsInParent().getMinX() - getParent().getLayoutBounds().getMinX();
+        if (offset < 0) {
+            getLocation().x -= offset;
+            getVelocity().x *= -1;
+        }
+
+        // lower wall
+        offset = getBoundsInParent().getMaxY() - getParent().getLayoutBounds().getMaxY();
+        if (offset > 0) {
+            getLocation().y -= offset;
+            getVelocity().y *= -1;
+        }
+
+        // upper wall
+        offset = getBoundsInParent().getMinY() - getParent().getLayoutBounds().getMinY();
+        if (offset < 0) {
+            getLocation().y -= offset;
+            getVelocity().y *= -1;
+        }
+    }
+
+
+}
