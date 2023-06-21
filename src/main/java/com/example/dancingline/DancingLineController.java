618963b76221d23c9ded9e9206d1616e45b7f256
DavidePaltrinieri <296723@studenti.unimore.it>
2023-06-21 13:34:34 +0200

    CubicLine inserita

--- src/main/java/com/example/dancingline/DancingLineController.java
+++ src/main/java/com/example/dancingline/DancingLineController.java
@@ -1,9 +1,13 @@
 package com.example.dancingline;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.shape.CubicCurve;
 import javafx.stage.Stage;
 
 public class DancingLineController {
 
+    @FXML
+    private CubicCurve cubicCurve;
 
 }
